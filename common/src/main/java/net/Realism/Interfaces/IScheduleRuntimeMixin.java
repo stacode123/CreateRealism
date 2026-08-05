1f098583542ea2af4836acacd6832877f0411988
stacode <stanislaw.lajszczak@gmail.com>
2026-08-05 21:30:45 +0200

    Refactor schedule runtime to support realistic train timetable mechanics.

--- common/src/main/java/net/Realism/Interfaces/IScheduleRuntimeMixin.java
+++ common/src/main/java/net/Realism/Interfaces/IScheduleRuntimeMixin.java
@@ -1,11 +1,11 @@
 package net.Realism.Interfaces;
 
 public interface IScheduleRuntimeMixin {
-    long getDepartureDate();
-    long getExpectedArrivalDate();
-    void setExpectedArrivalDate(long date);
-    int getLastScheduledDepartureDate();
-    void setLastScheduledDepartureDate(int date);
-    boolean dontCheck();
-    void setDontCheck(boolean dontCheck);
+    /** Game time at which the train started waiting at its current stop, or 0 if it never has. */
+    long getArrivalGameTime();
+
+    /** Game time of the departure slot this train last left on, or 0 if it has not left one yet. */
+    long getLastDepartureSlot();
+
+    void setLastDepartureSlot(long slot);
 }
